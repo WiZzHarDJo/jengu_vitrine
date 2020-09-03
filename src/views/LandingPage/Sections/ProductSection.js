1be8baa468e1882bde32a597a348e280b3241442
Joaquim <jogi@itu.dk>
2020-09-03 06:43:23 +0200

    Fix descr product mobile

--- src/views/LandingPage/Sections/ProductSection.js
+++ src/views/LandingPage/Sections/ProductSection.js
@@ -2,6 +2,11 @@ import React from "react";
 // @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
 
+import {
+  BrowserView,
+  MobileView
+} from "react-device-detect";
+
 // @material-ui/icons
 import Explore from "@material-ui/icons/Explore";
 import ImportantDevices from "@material-ui/icons/ImportantDevices";
@@ -23,6 +28,7 @@ export default function ProductSection() {
   const classes = useStyles();
   return (
     <div className={classes.section}>
+      <BrowserView>
       <GridContainer justify="center">
         <GridItem xs={12} sm={12} md={8}>
           <h2 className={classes.title}>Notre projet</h2>
@@ -91,6 +97,78 @@ export default function ProductSection() {
           </GridItem>
         </GridContainer>
       </div>
+      </BrowserView>
+      <MobileView>
+      <GridContainer justify="center">
+        <GridItem xs={12} sm={12} md={8}>
+          <h2 className={classes.title}>Notre projet</h2>
+        </GridItem>
+        <h5 className={classes.description} style={{textAlign: "justify", marginLeft: "15px", marginRight: "15px"}}>
+        Jengu est une plateforme innovante qui a pour volonté d'optimiser la recherche d'emploi des étudiants et jeunes diplômés en pharmacie,
+        et permettre aux officines de recruter <b>LE</b> candidat idéal. Jengu vous propose une solution inédite qui passe par un algorithme de correspondance
+        des profils étudiants et officinaux, permettant de vous offrir un service ultra-personnalisé qui correspond à chacune de vos attentes.
+        Jengu est également une plateforme de formation à destination des étudiants et des pharmaciens, car nous souhaitons vous accompagner au quotidien.
+        </h5>
+      </GridContainer>
+      <div style={{marginTop:"30px"}}>
+        <GridContainer>
+          <GridItem xs={12} sm={12} md={4}>
+            <InfoArea
+              title="Formateur"
+              description="Développez vous au quotidien grâce nos formations en ligne conçues pour tous !"
+              icon={Explore}
+              iconColor="info"
+              vertical
+            />
+          </GridItem>
+          <GridItem xs={12} sm={12} md={4}>
+            <InfoArea
+              title="Intuitif"
+              description="Rencontrez vos étudiants ou vos officines à travers une interface simple et intuitive !"
+              icon={ImportantDevices}
+              iconColor="success"
+              vertical
+            />
+          </GridItem>
+          <GridItem xs={12} sm={12} md={4}>
+            <InfoArea
+              title="Impliqué"
+              description="Soyez toujours à jour avec les dernières actualités pharmaceutiques à portée de main !"
+              icon={MenuBook}
+              iconColor="blue"
+              vertical
+            />
+          </GridItem><GridItem xs={12} sm={12} md={4}>
+            <InfoArea
+              title="Profils vérifiés"
+              description="Croisez des personnes certifiées à l'identité et aux diplômes vérifiés !"
+              icon={VerifiedUser}
+              iconColor="success"
+              vertical
+            />
+          </GridItem>
+          <GridItem xs={12} sm={12} md={4}>
+            <InfoArea
+              title="Offres complètes"
+              description="Publiez en quelques clics vos offres à travers une procédure simplifiée !"
+              icon={Speed}
+              iconColor="blue"
+              vertical
+            />
+          </GridItem>
+          <GridItem xs={12} sm={12} md={4}>
+            <InfoArea
+              title="En direct"
+              description="Discutez sans intermédiaire et immédiatement avec l'étudiant ou l'officine !"
+              icon={Forum}
+              iconColor="info"
+              vertical
+            />
+          </GridItem>
+        </GridContainer>
+      </div>
+      </MobileView>
+
     </div>
   );
 }
